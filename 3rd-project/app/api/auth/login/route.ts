2d5577f5046228e12f3d460a2d80906a3658e74f
melkamu ayalew <melkamuayal21@gmail.com>
2025-08-01 15:31:38 +0300

    commited the add part

--- 3rd-project/app/api/auth/login/route.ts
+++ 3rd-project/app/api/auth/login/route.ts
@@ -16,7 +16,7 @@ export async function POST(req: Request) {
       return NextResponse.json({ error: 'Invalid password' }, { status: 401 });
     }
 
-    const token = signToken({ userId: user.id });
+    const token = signToken({ id: user.id, email: user.email, name: user.name });
 
     return NextResponse.json({ message: 'Login successful', token });
   } catch (error) {
